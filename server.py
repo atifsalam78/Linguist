abe085ec020e2a1a5e639eb903e409ebd78892e8
Atif Salam <98662990+atifsalam78@users.noreply.github.com>
2023-09-06 01:57:12 +0500

    Add files via upload

--- server.py
+++ server.py
@@ -1,24 +1,33 @@
 from flask import Flask, render_template, request # This is different from requests
-from MyTranslator import translate_text
+# from MyTranslator import translate_text
 import SpeechTranslator_my as st
 from waitress import serve
 from googletrans import Translator
 from gtts import gTTS
 import os
 from playsound import playsound
+import wikipedia
+
 
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/index")
 
+
 def index():    
-    return render_template("/index.html")
+    return render_template("index.html")
+
+@app.route("/about")
+def about():    
+    return render_template("about.html")
 
 @app.route("/getData")
-def get_data():    
+def get_data():
+    
     op = request.args.get("operation") # Select operation   
     source_text = request.args.get("source_text") # Source text given by the user for translation
+    
     target_lang = request.args.get("target_language") # Target language selected by the user
 
     if op == "S2ST" and bool(target_lang): # Speech To Speech Translation
@@ -36,8 +45,8 @@ def get_data():
 
         # Using OS module to run the translated voice.
         playsound('captured_voice.mp3')
-        os.remove('captured_voice.mp3')
-        print(text)
+        # os.remove('captured_voice.mp3')
+        # print(text)
         return render_template("index.html")
     
     elif op == "S2TT" and bool(target_lang): # Speech To Text Translation
@@ -50,6 +59,7 @@ def get_data():
                                translated_text = tran_text)       
     
     elif op == "T2ST" and bool(target_lang): # Text To Speech Translation
+
         text_trans = Translator()
         text_to_translate = text_trans.translate(source_text, dest=target_lang)
         tran_text = text_to_translate.text
@@ -63,7 +73,7 @@ def get_data():
 
         # Using OS module to run the translated voice.
         playsound('captured_voice.mp3')
-        os.remove('captured_voice.mp3')
+        # os.remove('captured_voice.mp3')
         # print(text)
 
         return render_template("index.html",
@@ -86,5 +96,5 @@ def get_data():
 
 if __name__ == "__main__":
 
-    app.run(host="0.0.0.0", port = 8000) # code for run on local machine
+    app.run(host="0.0.0.0", port = 8000, debug=True) # code for run on local machine
     # serve(app, host="0.0.0.0", port=8000) # code for run in production server

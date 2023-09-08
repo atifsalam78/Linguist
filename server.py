b27a36a95bb22df9b73eb38ed9ac08b7cf765ccd
Atif Salam <98662990+atifsalam78@users.noreply.github.com>
2023-09-08 20:34:21 +0500

    Add files via upload

--- server.py
+++ server.py
@@ -1,12 +1,10 @@
 from flask import Flask, render_template, request # This is different from requests
-# from MyTranslator import translate_text
-import SpeechTranslator_my as st
+import SpeechTranslator as st
 from waitress import serve
 from googletrans import Translator
 from gtts import gTTS
 import os
 from playsound import playsound
-import wikipedia
 
 
 app = Flask(__name__)
@@ -22,78 +20,87 @@ def index():
 def about():    
     return render_template("about.html")
 
+
+@app.route("/error_page")
+def error_page():
+    return render_template("error_page.html")
+
 @app.route("/getData")
-def get_data():
-    
-    op = request.args.get("operation") # Select operation   
-    source_text = request.args.get("source_text") # Source text given by the user for translation
-    
-    target_lang = request.args.get("target_language") # Target language selected by the user
-
-    if op == "S2ST" and bool(target_lang): # Speech To Speech Translation
-        source_text = st.takeinput()
-
-        audio_trans_text = Translator()
-        text_to_translate = audio_trans_text.translate(source_text, dest=target_lang)
-        text = text_to_translate.text
-
-        speak = gTTS(text=text, lang=target_lang, slow=False)        
-
-        # Using save() method to save the translated
-        # speech in capture_voice.mp3
-        speak.save("captured_voice.mp3")
-
-        # Using OS module to run the translated voice.
-        playsound('captured_voice.mp3')
-        # os.remove('captured_voice.mp3')
-        # print(text)
-        return render_template("index.html")
-    
-    elif op == "S2TT" and bool(target_lang): # Speech To Text Translation
-        source_text = st.takeinput()
-        audio_trans_text = Translator()
-        text_to_translate = audio_trans_text.translate(source_text, dest=target_lang)
-        tran_text = text_to_translate.text
-
-        return render_template("index.html",
-                               translated_text = tran_text)       
-    
-    elif op == "T2ST" and bool(target_lang): # Text To Speech Translation
-
-        text_trans = Translator()
-        text_to_translate = text_trans.translate(source_text, dest=target_lang)
-        tran_text = text_to_translate.text
+def get_data(): # Connect and Playsound error
+    try:
+        op = request.args.get("operation") # Select operation   
+        source_text = request.args.get("source_text") # Source text given by the user for translation
         
+        target_lang = request.args.get("target_language") # Target language selected by the user
+
+        if op == "S2ST" and bool(target_lang): # Speech To Speech Translation
+            source_text = st.takeinput()
+            audio_trans_text = Translator()
+            text_to_translate = audio_trans_text.translate(source_text, dest=target_lang)
+            tran_text = text_to_translate.text
+
+            speak = gTTS(text=tran_text, lang=target_lang, slow=False)        
+
+            # Using save() method to save the translated
+            # speech in capture_voice.mp3
+            speak.save("captured_voice.mp3")
+
+            # Using OS module to run the translated voice.
+            playsound('captured_voice.mp3')
+            os.remove('captured_voice.mp3')
+            # print(text)
+            return render_template("index.html",
+                                translated_text = tran_text)
         
-        speak = gTTS(text=tran_text, lang=target_lang, slow=False)        
+        # elif op == "S2TT" and bool(target_lang): # Speech To Text Translation
+        elif op == "S2TT": # Speech To Text Translation
+            source_text = st.takeinput()
+            audio_trans_text = Translator()
+            text_to_translate = audio_trans_text.translate(source_text, dest=target_lang)
+            tran_text = text_to_translate.text
 
-        # Using save() method to save the translated
-        # speech in capture_voice.mp3
-        speak.save("captured_voice.mp3")
+            return render_template("index.html",
+                                translated_text = tran_text)       
+        
+        elif op == "T2ST" and bool(target_lang): # Text To Speech Translation
 
-        # Using OS module to run the translated voice.
-        playsound('captured_voice.mp3')
-        # os.remove('captured_voice.mp3')
-        # print(text)
+            text_trans = Translator()
+            text_to_translate = text_trans.translate(source_text, dest=target_lang)
+            tran_text = text_to_translate.text
+            
+            
+            speak = gTTS(text=tran_text, lang=target_lang, slow=False)        
 
-        return render_template("index.html",
-                               translated_text = tran_text)
-    
-    elif (op == "T2TT" and bool(target_lang)) and bool(source_text.strip()): # Text To Text Translation       
+            # Using save() method to save the translated
+            # speech in capture_voice.mp3
+            speak.save("captured_voice.mp3")
 
-        text_trans = Translator()
-        text_to_translate = text_trans.translate(source_text, dest=target_lang)
-        tran_text = text_to_translate.text
+            # Using OS module to run the translated voice.
+            playsound('captured_voice.mp3')
+            os.remove('captured_voice.mp3')
+            # print(text)
 
-        return render_template("index.html",
-                               translated_text = tran_text)
-    elif op == "ASR":
-        return f"{op} Under Construction"
+            return render_template("index.html",
+                                translated_text = tran_text)
+        
+        elif (op == "T2TT" and bool(target_lang)) and bool(source_text.strip()): # Text To Text Translation       
 
-    else:
-        return render_template ("index.html",
-                                 translated_text = 'Provide "Operation"/"Target Language"/"Text"')
+            text_trans = Translator()
+            text_to_translate = text_trans.translate(source_text, dest=target_lang)
+            tran_text = text_to_translate.text
 
+            return render_template("index.html",
+                                translated_text = tran_text)
+        elif op == "ASR":
+            return f"{op} Under Construction"
+
+        else:
+            return render_template ("index.html",
+                                    translated_text = 'Provide "Operation"/"Target Language"/"Text"')
+    except Exception as e:
+        return render_template ("error_page.html",
+                                error_input = str(e).capitalize())
+                                    
 if __name__ == "__main__":
 
     app.run(host="0.0.0.0", port = 8000, debug=True) # code for run on local machine
